8ae30f56968e118d7cb1df3fe7a673452ab638b7
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2023-07-02 05:14:21 -0400

    Create sorting.js

---
+++ Sorting/sorting.js
@@ -0,0 +1,6 @@
+const letters = ['d', 'c', 'a', 'x', 't']
+const basket = [2, 65, 34, 2, 3, 20]
+
+console.log(letters.sort());
+sorted_numbers = basket.sort(function (a, b) { return a - b; });
+console.log(sorted_numbers);
