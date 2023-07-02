9ddcf79ab8daff7fb55e64ab1a54dfc885bea977
y0usSs3F <120474463+y0usSs3F@users.noreply.github.com>
2023-07-02 05:08:34 -0400

    Create finding_Nemo.js

---
+++ finding_Nemo.js
@@ -0,0 +1,15 @@
+//#1 -- For loop in Javascript.
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(100000).fill('nemo');
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO! ' + i);
+    }
+  }
+}
+
+findNemo(large)
